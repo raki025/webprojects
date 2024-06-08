78a237967844d1aa06630fbdb3de21dc75a37242
rakesh025-hub <108419721+rakesh025-hub@users.noreply.github.com>
2024-06-08 20:31:49 +0530

    usingswitch

--- Drumkit/index.js
+++ Drumkit/index.js
@@ -1,18 +1,57 @@
+//selecting class .drum to target all buttons
 
 for (var i=0; i<document.querySelectorAll(".drum").length;i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", clickhandle)
+
+    // this function extract class string 
 function clickhandle(){
-    var whichbtn = this.innerHTML
+    var whichbtn = this.innerHTML;
     console.log(whichbtn)
-   if (whichbtn == "w"){
-    var sound = new Audio("sounds/crash.mp3")
-    sound.play()
-   }
-   else if(whichbtn == "a") {
-    var sound = new Audio("sounds/kick-bass.mp3")
-    sound.play()
+    //playing sound for each button whith string got from whichbtn
+   switch (whichbtn) {
+    case "w":
+        var sound1 = new Audio("sounds/crash.mp3")
+        sound1.play()
+        break;
+    
+
+        case "a":
+        var sound2 = new Audio("sounds/kick-bass.mp3");
+        sound2.play();
+        break;
+
+        case "s":
+        var sound3 = new Audio("sounds/snare.mp3");
+        sound3.play();
+        break;
+
+        case "d":
+        var sound4 = new Audio("sounds/tom-1.mp3");
+        sound4.play();
+        break;
+
+        case "j":
+        var sound5 = new Audio("sounds/tom-2.mp3");
+        sound5.play();
+        break;
+
+        case "k":
+        var sound6 = new Audio("sounds/tom-3.mp3");
+        sound6.play();
+        break;
+
+        case "l":
+        var sound7 = new Audio("sounds/tom-4.mp3");
+        sound7.play();
+        break;
+    default:
+        alert("error");
+        break;
    }
    
 }
 }
+
+var sound = new Audio("sounds/kick-bass.mp3")
+sound.play()
