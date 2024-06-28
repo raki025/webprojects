9971ea50acd43a070adbff3c606ec2fde24656ed
rakesh025-hub <108419721+rakesh025-hub@users.noreply.github.com>
2024-06-28 22:58:21 +0530

    on280624_suggestio_date_quote_working_littlechange

--- backend/suggestion/app.js
+++ backend/suggestion/app.js
@@ -7,22 +7,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app =express();
 const port = 3000
 
-const datedays = new Date("June 25, 2024 11:11:00");
+const datedays = new Date("June 30, 2024 12:05:01");
 const days = datedays.getDay();
 
-var datp = "weekday";
-var qu2 = " go to work";
 
+
+var datp = "";
+var qu2 = "";
 if (days == 0 || days == 6){
     datp = " its weekend";
     qu2= "enjoy";
+}else{
+    datp = " its weekday";
+    qu2= "go to work";
 }
 
 
 app.get("/", (req,res)=>{
     res.render("index.ejs",{
         daytype : datp,
-        quote: qu2
+        quote: qu2,
+        dat1: datedays,
 
     });
 })
